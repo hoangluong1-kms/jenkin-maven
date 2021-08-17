09053a1364b1e9c851e184ccdc24278343a17e3a
hoangluong1-kms <hoangluong1@kms-technology.com>
2021-08-18 00:36:13 +0700

    asd

--- TestMaven/src/main/java/com/example/demo/Hoang.java
+++ TestMaven/src/main/java/com/example/demo/Hoang.java
@@ -3,4 +3,22 @@ package com.example.demo;
 public class Hoang
 {
 
+    private int Id;
+
+    public Hoang(int id)
+    {
+        super();
+        Id = id;
+    }
+
+    public int getId()
+    {
+        return Id;
+    }
+
+    public void setId(int id)
+    {
+        Id = id;
+    }
+    
 }
