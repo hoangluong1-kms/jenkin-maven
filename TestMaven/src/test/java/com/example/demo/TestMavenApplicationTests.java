504c57e8c74daad70b524a982419e4786930f4e6
hoangluong1-kms <hoangluong1@kms-technology.com>
2021-08-17 23:54:17 +0700

    asdsad

--- TestMaven/src/test/java/com/example/demo/TestMavenApplicationTests.java
+++ TestMaven/src/test/java/com/example/demo/TestMavenApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
