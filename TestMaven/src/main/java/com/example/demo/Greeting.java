b2fa0c74e9982a0f8f8ab1fc3033ae3506bb2929
hoangluong1-kms <hoangluong1@kms-technology.com>
2021-08-17 23:34:46 +0700

    asdsad

--- TestMaven/src/main/java/com/example/demo/Greeting.java
+++ TestMaven/src/main/java/com/example/demo/Greeting.java
@@ -3,12 +3,12 @@ package com.example.demo;
 public class Greeting {
 
     private final long id;
-    private final String content; 
+    private final String content;  
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content; 
-    }
+    }d
 
     public long getId() {
         return id;
